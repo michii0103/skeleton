ca91bc982c47990d8f89a8e9a202805114c48885
michii <xuzrIp-hiwcot-0wegty>
2024-12-14 22:53:47 +0100

    chore: set port explicit in pm2 config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,9 +5,11 @@ module.exports = {
         watch: false,
         env: {
             NODE_ENV: 'development',
+            PORT: 8080, // Sicherstellen, dass der Port korrekt ist
         },
         env_production: {
             NODE_ENV: 'production',
+            PORT: 8080, // Sicherstellen, dass der Port korrekt ist
         },
     }, ],
 };
