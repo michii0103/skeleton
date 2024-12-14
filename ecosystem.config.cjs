c0a20f6308f59156f92dbe335b73486aff58974b
michii <xuzrIp-hiwcot-0wegty>
2024-12-14 22:37:23 +0100

    chore: pm2 config changes

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,18 +1,13 @@
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(
-    import.meta.url);
-const __dirname = dirname(__filename);
-
-export default {
+module.exports = {
     apps: [{
-        name: "skeleton",
-        script: "./server.js",
-        instances: 1,
-        exec_mode: "fork",
+        name: 'skeleton',
+        script: '/root/skeleton/dist/server/server.js',
+        watch: false,
         env: {
-            NODE_ENV: "production",
+            NODE_ENV: 'development',
+        },
+        env_production: {
+            NODE_ENV: 'production',
         },
     }, ],
 };
