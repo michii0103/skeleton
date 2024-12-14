54276d75275157469e20654b68ed88f76e37134e
michii <xuzrIp-hiwcot-0wegty>
2024-12-14 22:54:37 +0100

    chore: relative path to server.js in pm2 config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
         name: 'skeleton',
-        script: '/root/skeleton/dist/server/server.js',
+        script: './dist/server/server.js',
         watch: false,
         env: {
             NODE_ENV: 'development',
