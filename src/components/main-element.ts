27da5d90b026c66dd5c6d1eb0a5fb40ddb165386
michii <xuzrIp-hiwcot-0wegty>
2024-12-15 14:35:31 +0100

    refactor: clean up working tree

--- src/components/main-element.ts
+++ src/components/main-element.ts
@@ -1,8 +1,5 @@
 import { LitElement, html, css } from "lit";
 import { customElement, property } from "lit/decorators.js";
-import "./prosemirror-editor";
-import "./x-button";
-import { socket } from "../main";
 
 @customElement("main-element")
 export class MainElement extends LitElement {
@@ -21,27 +18,6 @@ export class MainElement extends LitElement {
   `;
 
   render() {
-    return html`
-      asdasdasdsad
-      <div class="message">${this.message}</div>
-      <x-button
-        @click=${() => {
-          socket.send(
-            JSON.stringify({ event: "hello", data: "Custom button press!" })
-          );
-        }}
-      >
-        Mein Custom Button
-      </x-button>
-      asdasdasd asd
-    `;
+    return html` Space for future projects... `;
   }
 }
-
-// Erzwingt einen vollständigen Reload, wenn HMR fehlschlägt
-// if (import.meta.env.DEV && import.meta.hot) {
-//   import.meta.hot.accept(() => {
-//     console.log("Reloading due to changes in main-element.ts");
-//     window.location.reload();
-//   });
-// }
